10a94e80f6d58d70620af0123db071cafb65d072
zhyio <wenaunxi7001@gmail.com>
2026-07-13 11:30:55 +0800

    fix generated icon production loading

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fitlog-v7'
+const CACHE = 'fitlog-v8'
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['./', './manifest.webmanifest', './icon.svg']))).then(() => self.skipWaiting()))
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))).then(() => self.clients.claim()))
 self.addEventListener('fetch', event => {
