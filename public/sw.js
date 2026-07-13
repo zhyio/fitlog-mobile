bd859686fa04e8399eaa26845568d5a57c68f7f3
zhyio <wenaunxi7001@gmail.com>
2026-07-13 16:54:30 +0800

    replace app theme icon

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'fitlog-v8'
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['./', './manifest.webmanifest', './icon.svg']))).then(() => self.skipWaiting()))
+const CACHE = 'fitlog-v9'
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['./', './manifest.webmanifest', './favicon.png', './apple-touch-icon.png', './icon-192.png', './icon-512.png']))).then(() => self.skipWaiting()))
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))).then(() => self.clients.claim()))
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return
